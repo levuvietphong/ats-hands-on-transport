ed7c00cbf007b2f2f1a4efd46b14531eef78d8b7
Phong Le <levuvietphong@gmail.com>
2024-11-08 22:35:05 -0500

    add integrated and extract transport runs, including changes in notebooks

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 # Amanzi-ATS Transport Tutorial
 
-This hands-on tutorial offers a hands-on experience in setting up and running transport simulations based on hydrology models in [Amanzi-ATS](https://github.com/amanzi/ats). This is part of the IDEAS-Watershed All-hands Meeting 2024 in Denver, CO.
+This hands-on tutorial offers a hands-on experience in setting up and running transport simulations based on hydrology models in [Amanzi-ATS](https://github.com/amanzi/ats). This is part of the [IDEAS-Watershed](https://ideas-watersheds.github.io/) all-hands meeting 2024 in Denver, CO.
 
-**Facilitators:** Phong Le, Ethan Coon, Daniil Svyatsky
+**Instructors:** Phong Le, Ethan Coon, Daniil Svyatsky
 
 ---
 
-### 1. Learning Objectives
+### 1. Learning Objectives :dart:
 By the end of this workshop, participants will be able to:
-* Understand core concepts of the [transport process kernel (PK)](https://github.com/amanzi/ats/tree/master/src/pk_transport) in Amanzi-ATS
-* Develop workflows that integrate transport processes into [hydrology](https://amanzi.github.io/ats/stable/input_spec/process_kernels/physical/flow.html) models
+* Understand key concepts of the [transport](https://github.com/amanzi/ats/tree/master/src/pk_transport) process kernel (PK) in Amanzi-ATS;
+* Develop workflows that integrate transport PK into [hydrology](https://amanzi.github.io/ats/stable/input_spec/process_kernels/physical/flow.html) PK.
 
 ---
 
-### 2. Prerequisites
+### 2. Prerequisites :bulb:
 To get the most out of this tutorial, participants should have:
-* Basic skills in Python and familiarity with [Jupyter Notebook](https://jupyter.org/). We will use [PyVista](https://pyvista.org/) for visualization.
+* Basic skills in [python](https://www.python.org/) and [jupyter notebook](https://jupyter.org/). We will use [pyvista](https://pyvista.org/) and [ipywidgets](https://ipywidgets.readthedocs.io/) for interactive visualization.
 * A fundamental understanding of watershed hydrology is encouraged.
 
 ---
-### 3. Setup Instructions
+### 3. Setup Instructions :wrench:
+The tutorial will be conducted in a Docker container. The docker includes the `terminal`, `text editor`, `ats`, `python`, `jupyter notebook`, and other dependencies. The `text editor` is customized to better work with the `ats` input files in `xml` format. Follow the steps below to set up the environment.
 1. **Pull the Docker Image:**
 Make sure to pull the Docker container for this tutorial:
 ```bash
@@ -32,17 +33,26 @@ Start the container and open the Jupyter Notebook interface:
 docker run --rm -it -p 8888:8888 metsi/ideas-watersheds-all-hands-2024:v0
 ```
 
-3. **Access Jupyter Notebooks:**
-Open the link displayed in your terminal to access Jupyter Notebook in your browser.
+3. **Access Jupyter notebooks:**
+Open the link displayed in your terminal to access `jupyter notebook` in your browser. 
+
+4. **Clone the tutorial repository:**
+Open a terminal in the Jupyter notebook interface and run:
+```bash
+git clone https://github.com/amanzi/ats-hands-on-transport.git
+```
+
+> [!IMPORTANT]
+> If you're familiar with the `VSCode` editor, you can use the [Dev Containers](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers) extension to connect to the container and edit XML files. You can also use the [jupyter](https://marketplace.visualstudio.com/items?itemName=ms-toolsai.jupyter) extension in `VSCode` to run notebooks directly within the Docker container.
 
 ---
 
-### 4. Workshop Outline
-* **Introduction to ATS Hydrology and Transport:**  
-Overview of fundamental equations in hydrology and transport in Amanzi-ATS
+### 4. Workshop Outline :memo:
+1. **Introduction to ATS Hydrology and Transport:**
+   *Overview of fundamental equations in hydrology and transport in Amanzi-ATS*
 
-* **Hands-On Session: Building transport simulations:**
-Step-by-step guidance on developing transport simulations in ATS based on the integrated hydrology model.
+2. **Hands-On Session: Building Transport Simulations:**
+   *Step-by-step guidance on developing transport simulations in ATS based on the integrated hydrology model*
 
-* **Discussion and Feedbacks** 
-Open session for questions, insights, and feedback on the tutorial content.
+3. **Discussion and Feedback:**
+   *Open session for questions, insights, and feedback*
--- notebooks/libs/ats_vis.py
+++ notebooks/libs/ats_vis.py
@@ -7,6 +7,9 @@ from pyproj.crs import CRS
 from matplotlib import pyplot as plt
 from matplotlib.colors import Normalize, LogNorm
 from matplotlib.patches import Polygon  # Use this instead of PolygonPatch
+from matplotlib.ticker import MultipleLocator
+import xml.etree.ElementTree as ET
+
 import shapely
 import colorcet as cc
 import pyvista as pv
@@ -96,7 +99,8 @@ def GetMeshPolygons(mfile=None):
 
 
 def PlotPolygonsFields(polygons, field, cmap='jet', alpha=1, lw=1, edgecolor='k',
-                       vmin=None, vmax=None, label=None, colorbar=False, logscale=False, ax=None):
+                       vmin=None, vmax=None, label=None, colorbar=False, logscale=False,
+                       showbox=True, ax=None):
     """
     Plots Shapely polygons with colors based on the provided field values.
     """
@@ -107,6 +111,8 @@ def PlotPolygonsFields(polygons, field, cmap='jet', alpha=1, lw=1, edgecolor='k'
         vmin = min(field)
     if vmax is None:
         vmax = max(field)
+    if vmax < vmin:
+        vmax = vmin*2
 
     if ax is None:
         _, ax = plt.subplots()
@@ -126,17 +132,29 @@ def PlotPolygonsFields(polygons, field, cmap='jet', alpha=1, lw=1, edgecolor='k'
     for patch in patches:
         ax.add_patch(patch)
 
-    ax.set_axis_off()
-
     if colorbar:
         # Add colorbar if needed
         sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
-        cbar = plt.colorbar(sm, ax=ax, orientation='horizontal', pad=0.02)
+        if showbox:
+            cbar = plt.colorbar(sm, ax=ax, orientation='horizontal', pad=0.1)
+        else:
+            cbar = plt.colorbar(sm, ax=ax, orientation='horizontal', pad=0.01)
 
-        # Make colorbar ticks smarter
-        tick_locator = plt.MaxNLocator(nbins=5)
-        cbar.locator = tick_locator
-        cbar.update_ticks()
+        if logscale:
+            # For logscale, show all values from vmin to vmax
+            range_size = int(np.log10(vmax)) - int(np.log10(vmin)) + 1
+            step = 2 if range_size > 5 and range_size % 2 == 1 else 1
+            ticks = [pow(10, i) for i in range(
+                int(np.log10(vmin)),
+                int(np.log10(vmax))+1,
+                step
+            )]
+            cbar.set_ticks(ticks)
+        else:
+            # Make colorbar ticks smarter for linear scale
+            cbar.set_ticks(np.linspace(vmin, vmax, 5))
+            cbar.update_ticks()
+
         if label is not None:
             cbar.set_label(label)
     plt.tight_layout()
@@ -159,23 +177,62 @@ def get_cmap(name):
         matplotlib.colors.Colormap: The colormap object corresponding to the input name.
         If the name is not found in the predefined dictionary, it returns the 'jet' colormap as default.
     """
+    if isinstance(name, list):
+        return 'jet'
+
     cmap_dict = {
         'elevation': 'terrain',
-        'ponded_depth': cc.cm.CET_CBL2_r,
-        'surface_subsurface_flux': 'seismic'
+        'ponded_depth': cc.cm.CET_L17,
+        'surface_subsurface_flux': 'seismic',
+        'pressure': cc.cm.CET_R4,
+        'pres_elev': cc.cm.CET_R4,
     }
     return cmap_dict.get(name, 'jet')
 
 
 def get_boundary_polygons(surface_polys):
+    """Get the outer boundary of a collection of surface polygons.
+
+    Args:
+        surface_polys (list): List of surface polygons
+
+    Returns:
+        GeoDataFrame: Single polygon representing the outer boundary
+    """
     gdf = gpd.GeoDataFrame(geometry=surface_polys)
     gdf.crs = daymet_crs()
     gdf_union = gdf.union_all()
     return gpd.GeoDataFrame(crs=gdf.crs, geometry=[gdf_union])
 
 
+def format_box(ax, gdf_clip, showbox, colorbox):
+    """Format plot axes with consistent styling.
+
+    Args:
+        ax: Matplotlib axis object
+        gdf_clip: GeoDataFrame with boundary information
+        showbox (bool): Whether to show the axis box and labels
+        colorbox (str): Color for the axis box
+    """
+    ax.set_aspect('equal')
+    if showbox:
+        ax.spines['top'].set_color(colorbox)
+        ax.spines['bottom'].set_color(colorbox)
+        ax.spines['left'].set_color(colorbox)
+        ax.spines['right'].set_color(colorbox)
+        ax.tick_params(axis='both', which='major', labelsize=7)            
+        ax.set_xlabel('X (m)')
+        ax.set_ylabel('Y (m)')
+        xmin, ymin, xmax, ymax = gdf_clip.total_bounds
+        ax.set_xlim(xmin, xmax)
+        ax.set_ylim(ymin, ymax)
+    else:
+        ax.set_axis_off()
+
+
 def plot_surface(variables, poly_surface, vis_surface, step, time, domain='surface',
-                 vmin=None, vmax=None, cmap=None, units=None, logscales=None):
+                 vmin=None, vmax=None, cmap=None, units=None, logscales=None, figsize=None,
+                 colorbox='whitesmoke', showbox=True):
     """
     Plot surface variables on a mesh.
 
@@ -188,40 +245,46 @@ def plot_surface(variables, poly_surface, vis_surface, step, time, domain='surfa
         vmax (list, optional): List of maximum values for color scaling.
         cmap (list, optional): List of colormaps for each variable.
     """
+    
     num_plots = len(variables)
-    gdf_clip = get_boundary_polygons(poly_surface)
+    if figsize is None:
+        figsize = (4*num_plots, 6)
 
-    _, ax = plt.subplots(1, num_plots, sharex=True, sharey=True, figsize=(4*num_plots, 6))
+    _, ax = plt.subplots(1, num_plots, figsize=figsize)
     plt.suptitle(f'Time: {time[step]/vis_surface.time_factor} {vis_surface.time_unit}', fontsize=16)
+
+    if num_plots == 1:
+        ax = [ax]
+
+    # Plot each variable
+    gdf_clip = get_boundary_polygons(poly_surface)
     for k, var in enumerate(variables):
         if isinstance(var, list):
             data1 = vis_surface.getArray(domain+'-'+var[0])[step, :]
             data2 = vis_surface.getArray(domain+'-'+var[1])[step, :]
             data = data1 * data2
-            ax[k].set_title(f'{var[0]} * {var[1]}', y=0.95, fontsize=14)
+            ax[k].set_title(f'{var[0]} * {var[1]}', y=1.0, fontsize=14)
         else:
             data = vis_surface.getArray(domain+'-'+var)[step, :]
-            ax[k].set_title(var, y=0.95, fontsize=14)
-        
-        colormap = cmap[k] if cmap[k] is not None else get_cmap(var)
-        cmin = vmin[k] if vmin is not None else None
-        cmax = vmax[k] if vmax is not None else None
+            ax[k].set_title(var, y=1.0, fontsize=12)
 
-        if cmin is None:
-            cmin = np.floor(np.nanmin(data))
-        if cmax is None:
-            cmax = np.ceil(np.nanmax(data))
+        colormap = cmap[k] if cmap and cmap[k] else get_cmap(var)
+        cmin = vmin[k] if vmin and vmin[k] is not None else np.floor(np.nanmin(data))
+        cmax = vmax[k] if vmax and vmax[k] is not None else np.ceil(np.nanmax(data))
         unit = units[k] if units is not None else None
         logscale = logscales[k] if logscales is not None else False
-
+        if logscale:
+            cmin = 1e-8 if cmin == 0 else cmin
+            cmax = cmin*10 if cmax == 0 else cmax
         PlotPolygonsFields(poly_surface, data, cmap=colormap, vmin=cmin, vmax=cmax, lw=0.2,
-                           ax=ax[k], label=unit, colorbar=True, logscale=logscale)
-        ax[k].set_axis_off()
+                           ax=ax[k], label=unit, colorbar=True, logscale=logscale, showbox=showbox)
+        format_box(ax[k], gdf_clip, showbox, colorbox)
         gdf_clip.plot(ax=ax[k], color="None", edgecolor='k', lw=1)
 
 
 def plot_domain(variables, poly_surface, vis_domain, num_surface_elements, step, time, layer,
-                vmin=None, vmax=None, cmap=None, units=None, logscale=False):
+                vmin=None, vmax=None, cmap=None, units=None, logscale=False, figsize=None,
+                colorbox='whitesmoke', showbox=True):
     """
     Plot domain variables on a mesh.
 
@@ -234,30 +297,164 @@ def plot_domain(variables, poly_surface, vis_domain, num_surface_elements, step,
         vmax (list, optional): List of maximum values for color scaling.
         cmap (list, optional): List of colormaps for each variable.
     """
+
     num_plots = len(variables)
-    gdf_clip = get_boundary_polygons(poly_surface)
     num_steps = len(vis_domain.cycles)
     num_layers = int(np.shape(vis_domain.centroids)[0] / num_surface_elements)
 
-    _, ax = plt.subplots(1, num_plots, sharex=True, sharey=True, figsize=(4*num_plots, 6))
-    plt.suptitle(f'Time: {time[step]/vis_domain.time_factor} {vis_domain.time_unit}', fontsize=16)
-    for k, var in enumerate(variables):
-        data = vis_domain.getArray(var).reshape(num_steps, num_surface_elements, num_layers)[step, :, layer]
-        colormap = cmap[k] if cmap is not None else get_cmap(var)
-        cmin = vmin[k] if vmin is not None else None
-        cmax = vmax[k] if vmax is not None else None
+    if figsize is None:
+        figsize = (4*num_plots, 6)
 
-        if cmin is None:
-            cmin = np.floor(np.nanmin(data))
-        if cmax is None:
-            cmax = np.ceil(np.nanmax(data))
+    _, ax = plt.subplots(1, num_plots, sharex=True, sharey=True, figsize=figsize)
+    plt.suptitle(f'Time: {time[step]/vis_domain.time_factor} {vis_domain.time_unit}', fontsize=16)
+
+    if num_plots == 1:
+        ax = [ax]
+
+    gdf_clip = get_boundary_polygons(poly_surface)
+    for k, var in enumerate(variables):
+        data = vis_domain.getArray(var).reshape(
+            num_steps, num_surface_elements, num_layers)[step, :, layer]
+
+        colormap = cmap[k] if cmap and cmap[k] else get_cmap(var)
+        cmin = vmin[k] if vmin and vmin[k] is not None else np.floor(np.nanmin(data))
+        cmax = vmax[k] if vmax and vmax[k] is not None else np.ceil(np.nanmax(data))
         unit = units[k] if units is not None else None
 
         PlotPolygonsFields(poly_surface, data, cmap=colormap, vmin=cmin, vmax=cmax, lw=0.2,
-                           ax=ax[k], label=unit, colorbar=True, logscale=logscale)
-        ax[k].set_title(var, y=0.95, fontsize=14)
-        ax[k].set_axis_off()
+                           ax=ax[k], label=unit, colorbar=True, logscale=logscale, showbox=showbox)
+        ax[k].set_title(var, y=1, fontsize=12)
+        format_box(ax[k], gdf_clip, showbox, colorbox)
         gdf_clip.plot(ax=ax[k], color="None", edgecolor='k', lw=1)
+
+
+def plot_sources(time, data, label=None, sfactor=1, color='dimgray', fontsize=12, xlabel=None,
+                 ylabel=None, title=None, legend=False, yscale=0.2, ax=None):
+    """Plot source data (like rainfall and tracer injection) on a secondary y-axis.
+
+    Parameters
+    ----------
+    time : array-like
+        Time values for x-axis
+    data : array-like
+        Source data to plot
+    label : str
+        Label for the data series
+    sfactor : float, optional
+        Scale factor for data values, default=1
+    color : str, optional
+        Color for the plot, default='dimgray'
+    fontsize : int, optional
+        Font size for labels, default=12
+    xlabel, ylabel : str, optional
+        Axis labels
+    title : str, optional
+        Plot title
+    legend : bool, optional
+        Whether to show legend, default=False
+    yscale : float, optional
+        Y-axis scale factor, default=0.2
+    ax : matplotlib.axes.Axes, optional
+        Primary axis to attach secondary axis to
+
+    Returns
+    -------
+    matplotlib.axes.Axes
+        The secondary axis containing the plot
+    """
+    # Add second y-axis
+    axs = ax.twinx()
+    axs.fill_between(time, data * sfactor, color=color, label=label, step='mid')
+    if xlabel is not None:
+        axs.set_xlabel(xlabel, fontsize=fontsize)
+    if ylabel is not None:
+        axs.set_ylabel(ylabel, fontsize=fontsize)    
+    if title is not None:
+        axs.set_title(title, fontsize=fontsize)
+
+    axs.invert_yaxis()
+    max_val = np.max(data)
+    magnitude = 10 ** np.floor(np.log10(max_val))
+    max_val = np.ceil(max_val / magnitude) * magnitude
+    axs.set_ylim(max_val/yscale, 0)  # Reversed since axis is inverted    
+    yticks = np.arange(0, 2*max_val + max_val/2, max_val/2)
+    axs.set_yticks(yticks)
+    axs.set_yticklabels([f'{int(y)}' for y in yticks])
+    if max_val >= 1000:  # Only use scientific notation for large numbers
+        axs.yaxis.set_major_formatter(plt.ScalarFormatter())
+        axs.yaxis.get_major_formatter().set_scientific(True)
+        axs.yaxis.get_major_formatter().set_powerlimits((-2, 3))
+
+    if legend:
+        axs.legend(fontsize=fontsize, frameon=True)
+
+    return axs
+
+
+def plot_field_timeseries(time, data, labels, sfactor=1, fontsize=12, xlabel='Time (hr)',
+                          ylabel=None, title=None, legend=True, ylim=None, logscale=False, ax=None):
+    """Plot time series data for multiple fields on a single axis.
+
+    Parameters
+    ----------
+    time : array-like
+        Time values for x-axis
+    data : list of array-like
+        List of data arrays to plot
+    labels : list of str
+        Labels for each data series
+    sfactor : float, optional
+        Scale factor to apply to data values, default=1
+    fontsize : int, optional
+        Base font size for plot text, default=12
+    xlabel : str, optional
+        Label for x-axis, default='Time (hr)'
+    ylabel : str, optional 
+        Label for y-axis, default=None
+    title : str, optional
+        Plot title, default=None
+    legend : bool, optional
+        Whether to show legend, default=True
+    ylim : tuple, optional
+        Y-axis limits (min, max), default=None
+    ax : matplotlib.axes.Axes, optional
+        Axes to plot on, creates new axes if None
+
+    Returns
+    -------
+    matplotlib.axes.Axes
+        The axes containing the plot
+    """
+    # Create a new axis if none is provided
+    if ax is None:
+        ax = plt.gca()
+    for label, field in zip(labels, data):
+        ax.plot(time, field * sfactor, label=label, linewidth=2)
+
+    ax.set_xlabel(xlabel, fontsize=fontsize)
+    ax.set_ylabel(ylabel, fontsize=fontsize)
+    ax.set_title(title, fontsize=fontsize+2)
+
+    ax.tick_params(axis='both', labelsize=fontsize)
+    ax.xaxis.set_major_locator(MultipleLocator(4))
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+    if ylim is not None:
+        ax.set_ylim(ylim)
+
+    max_val = np.max(data)
+    if max_val >= 1000 or max_val < 0.01:  # Use scientific notation for large and very small numbers
+        ax.yaxis.set_major_formatter(plt.ScalarFormatter())
+        ax.yaxis.get_major_formatter().set_scientific(True)
+        ax.yaxis.get_major_formatter().set_powerlimits((-2, 3))
+
+    if logscale:
+        ax.set_yscale('log')
+    ax.grid(True, linestyle='--', alpha=0.5)
+
+    if legend:
+        ax.legend(loc='center left', fontsize=fontsize, frameon=True)
+    
+    return ax
 
 
 def load_mesh_exodus(mesh_file, z_scale=1.0):
@@ -398,3 +595,78 @@ def plot_mesh(domain_mesh, opacity=1, show_edges=True, show_scalar_bar=True, pic
         pl.set_background("royalblue", top="aliceblue")
 
     pl.show()
+
+
+def get_rainfall_from_xml(xml_file, time_obs, sfactor=1, parameter='surface-water_source'):
+    """Extract rainfall data from an XML file and convert to observation times.
+
+    Parameters
+    ----------
+    xml_file : str
+        Path to XML file containing rainfall data
+    time_obs : array-like
+        Observation time points to interpolate rainfall to
+    sfactor : float, optional
+        Scale factor to apply to rainfall values, default=1
+
+    Returns
+    -------
+    array-like
+        Rainfall values interpolated to observation times
+    """
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    water_source = root.find(f".//ParameterList[@name='{parameter}']")
+    function = water_source.find(".//ParameterList[@name='function-tabular']")
+    x_time = [float(x) for x in function.find("Parameter[@name='x values']").attrib['value'].strip('{}').split(',')]
+    y_values = [float(y) for y in function.find("Parameter[@name='y values']").attrib['value'].strip('{}').split(',')]
+    num_time = len(x_time)
+
+    x_time = np.array(x_time)/3600
+    n = len(time_obs)
+    rainfall = np.zeros(n)
+    for i in range(num_time-1):
+        rainfall[(time_obs >= x_time[i]) & (time_obs <= x_time[i+1])] = y_values[i] * sfactor
+
+    return rainfall
+
+
+def get_tracer_source_from_xml(xml_file, time_obs, sfactor=1, parameter='source terms'):
+    """Extract tracer source data from an XML file and convert to observation times.
+
+    Parameters
+    ----------
+    xml_file : str
+        Path to XML file containing tracer source data
+    time_obs : array-like
+        Observation time points to interpolate source values to
+    sfactor : float, optional
+        Scale factor to apply to source values, default=1
+
+    Returns
+    -------
+    array-like
+        Tracer source values interpolated to observation times
+    """
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    
+    # Navigate to the tracer source section
+    source_terms = root.find(f".//ParameterList[@name='{parameter}']")
+    function = source_terms.find(".//ParameterList[@name='function-tabular']")
+    
+    # Extract time and value arrays
+    x_time = [float(x) for x in function.find("Parameter[@name='x values']").attrib['value'].strip('{}').split(',')]
+    y_values = [float(y) for y in function.find("Parameter[@name='y values']").attrib['value'].strip('{}').split(',')]
+    num_time = len(x_time)
+
+    # Convert time to hours
+    x_time = np.array(x_time)/3600
+
+    # Interpolate to observation times
+    n = len(time_obs)
+    source = np.zeros(n)
+    for i in range(num_time-1):
+        source[(time_obs >= x_time[i]) & (time_obs <= x_time[i+1])] = y_values[i] * sfactor
+
+    return source
